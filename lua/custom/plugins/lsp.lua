0a3757b4ff8ac777c21efaf02ee2f864c1c4f991
Donal Burns <donal.w.burns@hotmail.com>
2024-02-04 18:05:35 +0000

    Add mypy to Lsp

---
+++ lua/custom/plugins/lsp.lua
@@ -0,0 +1,19 @@
+-- Lsp plugins and config
+return {
+    -- community null ls package for non-lsp progs like mypy
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.diagnostics.mypy,
+                null_ls.builtins.formatting.codespell,
+            },
+            -- determine if none-ls should run on current buffer
+            should_attach = function(bufnr)
+                return not vim.api.nvim_buf_get_name(bufnr):match("^git://")
+            end,
+        })
+    end
+}
