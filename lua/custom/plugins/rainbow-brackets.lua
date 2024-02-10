7e388f912e87a62ccf2c5973bed2fc553afe2785
Donal Burns <donal.w.burns@hotmail.com>
2024-02-10 13:07:24 +0000

    Add rainbow paren/bracket

---
+++ lua/custom/plugins/rainbow-brackets.lua
@@ -0,0 +1,28 @@
+return {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+        local rainbow_delimiters = require("rainbow-delimiters")
+        require("rainbow-delimiters.setup").setup({
+            strategy = {
+                [""] = rainbow_delimiters.strategy["global"],
+                vim = rainbow_delimiters.strategy["local"],
+            },
+            query = {
+                [""] = "rainbow-delimiters",
+            },
+            priority = {
+                [""] = 110,
+                lua = 210,
+            },
+            highlight = {
+                "RainbowDelimiterYellow",
+                "RainbowDelimiterBlue",
+                "RainbowDelimiterOrange",
+                "RainbowDelimiterViolet",
+                "RainbowDelimiterCyan",
+                -- "RainbowDelimiterGreen",
+                "RainbowDelimiterRed",
+            },
+        })
+    end,
+}
