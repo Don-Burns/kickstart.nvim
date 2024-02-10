ab407f0b78febb2132016743009ce12024638666
Donal Burns <donal.w.burns@hotmail.com>
2024-02-10 17:17:19 +0000

    Add code samples dir
    Adding a dir for different languages that I can test lsp functionality, theme look etc. without needing to make files from scratch

---
+++ testing/codesamples/README.md
@@ -0,0 +1,1 @@
+Simple code samples to check behaviour of lsp and theme looks etc. on different languages.
---
+++ testing/codesamples/test.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+from os.path import abspath
+
+
+TOP_LEVEL_CONSTANT = 1
+
+
+class myClass:
+    def __init__(self):
+        ...
+
+
+def main() -> None:
+    """ """
+    variable: int = 123
+    dictionary = {
+        "key": "string value",
+    }
+    abspath("ssdfa")
+    # This is a comment
+
+    # TODO: this is a todo comment
+
+    # FIXME: this is a todo comment
+
+    # WARN: this is a todo comment
+
+    # ALT: this is a todo comment tyop
+
+    # ! this is a warning comment
+    variable = 1 + "str"
+    ## build a deep dictionary
+    d = {"a": {"a": {"a": {}}}}
+    a = True
+    b = None
+    if True:
+        print(variable)
+        if False:
+            print(variable)
+
+    print(variable)
+
+    def a():
+        print("a")
+
+        def b():
+            ...
+
+    for i in [1, 2, 3]:
+        print(i)
+
+    a = [
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        """""" """""" """""" """""" """""" """""",
+    ]
+    """ """
+    variable: int = 123
+    dictionary = {
+        "key": "string value",
+    }
+    abspath("ssdfa")
+    # This is a comment
+
+    # TODO: this is a todo comment
+
+    # FIXME: this is a todo comment
+
+    # WARN: this is a todo comment
+
+    # ALT: this is a todo comment typo
+
+    # ! this is a warning comment
+    variable = 1 + "str"
+
+    print(variable)
+
+    for i in [1, 2, 3]:
+        print(i)
+
+    a = [
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        """""" """""" """""" """""" """""" """""",
+    ]
+
+
+if __name__ == "__main__":
+    main()
