396378c5ffbac748c12912991432b4086239f2fc
Donal Burns <donal.w.burns@hotmail.com>
2026-05-24 12:07:36 +0100

    fix telescope search for v0.12

--- init.lua
+++ init.lua
@@ -156,7 +156,7 @@ require("lazy").setup({
   -- Fuzzy Finder (files, lsp, etc)
   {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
+    version = 'v2.2.0',
     dependencies = {
       "nvim-lua/plenary.nvim",
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
