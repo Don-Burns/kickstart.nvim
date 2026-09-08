99bf0886aeea166eab07694a918df94623107251
Donal Burns <donal.burns@renre.com>
2026-09-08 09:04:51 +0100

    feat: swap from nvim-cmp to blink for completion

--- lua/custom/plugins/autocomplete.lua
+++ lua/custom/plugins/autocomplete.lua
@@ -1,69 +1,56 @@
 return {
     -- Autocompletion
-    "hrsh7th/nvim-cmp",
+    "saghen/blink.cmp",
     dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
+        -- Snippet engine
         "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-
-        -- Adds LSP completion capabilities
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-path",
-
-        -- Adds a number of user-friendly snippets
-        -- "rafamadriz/friendly-snippets",
     },
-    config = function()
-        -- [[ Configure nvim-cmp ]]
-        -- See `:help cmp`
-        local cmp = require "cmp"
-        local luasnip = require "luasnip"
-        require("luasnip.loaders.from_vscode").lazy_load()
-        luasnip.config.setup {}
+    version = "1.*",
+    -- AND/OR build from source
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source with:
+    -- build = 'nix run .#build-plugin',
 
-        cmp.setup {
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            completion = {
-                completeopt = "menu,menuone,noinsert",
-            },
-            mapping = cmp.mapping.preset.insert {
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete {},
-                ["<CR>"] = cmp.mapping.confirm {
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                },
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_locally_jumpable() then
-                        luasnip.expand_or_jump()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            },
-            sources = {
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "path" },
-            },
-        }
-    end
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = { preset = "super-tab" },
+
+        appearance = {
+            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Adjusts spacing to ensure icons are aligned
+            nerd_font_variant = "mono"
+        },
+
+        -- (Default) Only show the documentation popup when manually triggered
+        completion = { documentation = { auto_show = false } },
+
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+        },
+
+        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+        --
+        -- See the fuzzy documentation for more information
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+
+        signature = { enabled = true },
+    },
+    opts_extend = { "sources.default" }
 }
