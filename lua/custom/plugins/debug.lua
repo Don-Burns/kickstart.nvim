cbbcbaafc026431f1be54c2a05a52c514f9f6da6
Donal Burns <donal.w.burns@hotmail.com>
2026-09-15 20:53:25 +0100

    fix: inverted F24/23 test binds

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -98,12 +98,12 @@ return {
             neotest.summary.open()
         end, { desc = "Test: Run project" })
         -- Also map F23/F24 for convenience on my keyboard 2nd layer
-        vim.keymap.set("n", "<F23>", function()
+        vim.keymap.set("n", "<F24>", function()
             neotest.run.run({ strategy = "dap" })
             -- don't need to check if open. already checks in open function
             neotest.summary.open()
         end, { desc = "Test: Run nearest test" })
-        vim.keymap.set("n", "<F24>", function()
+        vim.keymap.set("n", "<F23>", function()
             neotest.run.run(vim.fn.expand "%")
             -- don't need to check if open. already checks in open function
             neotest.summary.open()
